92e58a47b1f9e5909995041f6871342029115567
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2024-04-27 15:23:46 +0100

    added functionality to show how many years till the person becomes 80

---
+++ age_functions.py
@@ -0,0 +1,2 @@
+def till_80(age):
+  print(f"in {80-age} years time you will be 80")
--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,9 @@
+import age_functions
+
 print("Hello world!")
 
 name = input("What is your name")
 print(" Hello " + name + "!")
 age = input ("What is your age?")
 print("Your age is " + age )
+age_functions.till_80(age)
